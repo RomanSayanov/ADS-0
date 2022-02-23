1a34268eb4b0a4a04cd5bcfc7b643f48c3684ad8
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-02-23 21:23:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,15 +4,13 @@ int gcd(int a, int b) {
 	if (a <= b) {
 		for (int i = a; i != 0; i--) {
 			if (a % i == 0 && b % i == 0) {
-				cout << "NOD = " << i <<;
-				break;
+				return i;
 			}
 		}
 	} else {
 		for (int i = b; i != 0; i--) {
 			if (a % i == 0 && b % i == 0) {
-				cout << "NOD = " << i <<;
-				break;
+				return i;
 			}
 		}
 	}
