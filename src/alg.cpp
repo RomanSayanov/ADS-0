e2b734c6c2d7afa99271bcfbbbec600390333744
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-02-23 21:32:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,18 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	if (a <= b) {
-		for (int i = a; i != 0; i--) {
-			if (a % i == 0 && b % i == 0) {
-				return i;
-			}
-		}
-	} else {
-		for (int i = b; i != 0; i--) {
-			if (a % i == 0 && b % i == 0) {
-				return i;
-			}
-		}
-	}
-	return 1;
+ if (a <= b) {
+  for (int i = a; i != 0; i--) {
+   if (a % i == 0 && b % i == 0) {
+    return i;
+   }
+  }
+ } else {
+  for (int i = b; i != 0; i--) {
+   if (a % i == 0 && b % i == 0) {
+    return i;
+   }
+  }
+ }
+ return 1;
 }
