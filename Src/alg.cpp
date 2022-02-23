faf99c63c91ece0ddea29f52f3b537f8fc177118
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-02-23 18:31:44 +0300

    Update and rename src/alg.cpp to Src/alg.cpp

---
+++ Src/alg.cpp
@@ -0,0 +1,27 @@
+// Copyright 2021 NNTU-CS
+
+int gcd(int a, int b) {
+      if (a <= b)
+    {
+      for (int i = a; i != 0; i--)
+	{
+	  if (a % i == 0 && b % i == 0)
+	    {
+	      cout << "NOD = " << i << endl;
+	      break;
+	    }
+	}
+    }
+  else
+    {
+      for (int i = b; i != 0; i--)
+	{
+	  if (a % i == 0 && b % i == 0)
+	    {
+	      cout << "NOD = " << i << endl;
+	      break;
+	    }
+	}
+    }
+  return 1;
+}
